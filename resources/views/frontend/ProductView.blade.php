5517b7fc66df45ef4dbcc99db23bc2c1ff7535e1
asraf-mridha <asrafmridha@gmail.com>
2022-08-12 00:12:19 +0600

    Bootstarp table design for Product Show

---
+++ resources/views/frontend/ProductView.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="row mt-5">
+
+   
+        <div class="col-md-8 offset-md-2">
+ 
+   <div class="container">
+    <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#Sl No</th>
+            <th scope="col">Product Name</th>
+            <th scope="col">Product Category</th>
+            <th scope="col">Product Description</th>
+            <th scope="col">Price</th>
+            <th scope="col">Status</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+           
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          
+        </tbody>
+      </table>
+
+    </div>
+    </div>
+</div>
+
+
+    
+</body>
+</html>
