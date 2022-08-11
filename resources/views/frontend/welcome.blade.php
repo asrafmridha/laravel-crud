731bb13dfcb54f6affd84a53994f125fc76ed783
asraf-mridha <asrafmridha@gmail.com>
2022-08-11 22:05:39 +0600

    Create Product model

---
+++ resources/views/frontend/welcome.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+   
+
+<div class="row mt-5">
+
+   
+<div class="col-md-6 offset-md-3">
+
+
+<form method="POST" >
+<div class="form-group">
+<label for="exampleInputEmail1">Your Name</label>
+<input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name"  name="name">
+
+</div>
+<div class="form-group">
+<label for="exampleInputPassword1">Your Email</label>
+<input type="email" class="form-control" id="exampleInputPassword1" placeholder="Enter Email" name="email">
+</div>
+
+<div class="form-group">
+<label for="exampleInputPassword1">Status</label>
+<select class="form-control" name="status" id="">
+  <option value="#">---Select--</option>
+<option value="1">Active</option>
+<option value="2">Inactive</option>
+<option value="3">Suspend</option>
+</select>
+</div>
+
+<button name="save" type="submit" class="btn btn-primary mt-3">Submit</button>
+</form>
+
+</div>
+</div>
+
+</form>
+
+
+
+
+
+
+</body>
+</html>
