f27f91b48089cf8c78c65f6fdb5cae32e082b8bc
asraf-mridha <asrafmridha@gmail.com>
2022-08-12 09:03:30 +0600

    Create button for Add Product & Show Product

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <h1>This is Homepage</h1>
-    <a href="{{Route("addProductView")}}"> Add Product</a>
+    <h1 class="m-5">This is Homepage</h1>
+   <div class="m-5">
+
+    <a class="btn btn-primary" href="{{Route("addProductView")}}"> Add Product</a>
+
+    <a class="btn btn-primary " href="{{Route("ProductView")}}">Show Product</a>
+
+   </div>
 </body>
 </html>
