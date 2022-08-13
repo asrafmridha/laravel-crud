4e6a9f13a2eaa94208df31eded1804ee4222e083
asraf-mridha <asrafmridha@gmail.com>
2022-08-13 09:29:52 +0600

    Add Bootstarp Modal For delete Item View

--- resources/views/frontend/ProductView.blade.php
+++ resources/views/frontend/ProductView.blade.php
@@ -18,6 +18,14 @@
  
    <div class="container">
 
+    @if (Session::has('message'))
+    <div class="alert alert-success">
+        <ul>
+            <li>{{ Session::get('message') }}</li>
+        </ul>
+    </div>
+@endif
+
     <a href="{{Route("addProductView")}}" class="btn btn-primary"> Add Product</a>
     <table class="table table-striped table-dark mt-3">
         <thead>
@@ -47,9 +55,45 @@
             <td>{{$item->price}}</td>
             <td>{{$item->status}}</td>
             <td><a class="text-white" href=""><i class="fa-solid fa-square-pen"></i></a> </td>
-            <td><a class="text-white" href=""><i class="fa-solid fa-trash"></i></a></td>
+            <td><a  data-bs-toggle="modal" data-bs-target="#delete" class="text-white" ><i class="fa-solid fa-trash"></i></a></td>
           </tr>
           <?php $slno++; ?>
+
+          <!-- Modal -->
+<div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Confirmation Message</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+   Are You Sure Want to Delete this item?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a href="{{Route('deleteProduct',$item->id)}}"  type="button" class="btn btn-primary">Delete</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
           @endforeach
           
@@ -62,6 +106,16 @@
 </div>
 
 
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+@foreach ($data as $item)
+
+
+@endforeach
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
 </body>
 </html>
