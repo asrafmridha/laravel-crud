6e1d743dc1dd9c77d41096d12049337d13bbd742
asraf-mridha <asrafmridha@gmail.com>
2022-08-13 10:09:39 +0600

    add modal for update Product

--- resources/views/frontend/ProductView.blade.php
+++ resources/views/frontend/ProductView.blade.php
@@ -54,13 +54,13 @@
             <td>{{$item->pdesc}}</td>
             <td>{{$item->price}}</td>
             <td>{{$item->status}}</td>
-            <td><a class="text-white" href=""><i class="fa-solid fa-square-pen"></i></a> </td>
-            <td><a href="" data-bs-toggle="modal" data-bs-target="#delete" class="text-white" ><i class="fa-solid fa-trash"></i></a></td>
+            <td><a data-bs-toggle="modal" data-bs-target="#updateProduct" class="text-white" href=""><i class="fa-solid fa-square-pen"></i></a> </td>
+            <td><a href="" data-bs-toggle="modal" data-bs-target="#deleteProduct" class="text-white" ><i class="fa-solid fa-trash"></i></a></td>
           </tr>
           <?php $slno++; ?>
 
-          <!-- Modal -->
-<div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <!-- Modal For Delete -->
+<div class="modal fade" id="deleteProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -77,6 +77,76 @@
     </div>
   </div>
 </div>
+
+
+
+
+<!-- Modal For Update -->
+<div class="modal fade" id="updateProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Update Product</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <form method="POST" action="{{Route("addproduct")}}" >
+  
+          @csrf
+        
+        <div class="form-group">
+        <label for="exampleInputEmail1">Product Name</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Product Name"  name="pname">
+        
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Product Category</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Product Category"  name="pcategory">
+          
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Product Description</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Product Description"  name="pdesc">
+            
+            </div>
+        <div class="form-group">
+        <label for="exampleInputPassword1">Price</label>
+        <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Enter Price" name="price">
+        </div>
+        
+        <div class="form-group">
+        <label for="exampleInputPassword1">Status</label>
+        <select class="form-control" name="status" id="">
+          <option value="#">---Select--</option>
+        <option value="1">Active</option>
+        <option value="2">Inactive</option>
+        
+        </select>
+        </div>
+        
+       
+        </form>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Update</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
 
 
 
